23d1abf2a2b63cc1a61994fa489bd8a7380545cc
TASLIM-ENIOLLA <taslim.eniolla@gmail.com>
2022-09-20 04:39:54 +0100

    Fist commit 9/20/2022

--- components/pages/Landing.jsx
+++ components/pages/Landing.jsx
@@ -16,17 +16,30 @@ export default function Landing(){
 				<div className ="bd-example">
 					<div id="carouselOne" className ="carousel slide" data-ride="carousel">
 						<ol className ="carousel-indicators">
-							<li data-target="#carouselOne" data-slide-to="0" className =""></li>
+							<li data-target="#carouselOne" data-slide-to="0" className ="active"></li>
 							<li data-target="#carouselOne" data-slide-to="1" className =""></li>
-							<li data-target="#carouselOne" data-slide-to="2" className ="active"></li>
+							<li data-target="#carouselOne" data-slide-to="2" className =""></li>
 						</ol>
 						<div className ="carousel-inner">
+							<div className ="carousel-item bg_cover active" style = {{backgroundImage: `linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,.5)), url(/images/pp4.jpg)`}}>
+								<div className ="carousel-caption po-rel top-0 left-0 h-100 w-100 flex-v a-i-c j-c-c">
+									<div className ="container">
+										<div className ="row justify-content-center px-4">
+											<div className ="theme-thick-border-left py-4 col-lg-9 col-sm-12 text-left">
+												<h4 className = 'bold text-white resize-font'>PPI Innovative Grant 2022</h4>
+												<h5 className = 'text-white resize-font'>The One Voice Project for Nigeria</h5>
+												<a href = './apply-for-grant' style = {{border: '2px solid #ec3237'}} className = 'px-5 py-3 text-white bg-orange btn d-inline-block mt-5 text-uppercase fo-s-18'>apply now</a>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
 							<div className ="carousel-item bg_cover" style = {{backgroundImage: `url(/images/conference.jpg)`}}>
 								<div className ="carousel-caption po-rel top-0 left-0 h-100 w-100 flex-v a-i-c- j-c-c">
 									<div className ="container">
-										<div className ="row justify-content-center-">
-											<div className ="theme-thick-border-left col-lg-9 col-sm-12 text-left">
-												<h1 className = 'bold text-white resize-font'>PPI Grant 2022</h1>
+										<div className ="row justify-content-center px-4">
+											<div className ="theme-thick-border-left py-4 col-lg-9 col-sm-12 text-left">
+												<h4 className = 'bold text-white resize-font'>PPI Grant 2022</h4>
 												<h5 className = 'text-white resize-font'>Bootcamp starts in a bit</h5>
 												<a href = './bootcamp' style = {{border: '2px solid #ec3237'}} className = 'px-5 py-3 text-white bg-orange btn d-inlin-block mt-5 text-uppercase fo-s-18'>learn more</a>
 											</div>
@@ -34,25 +47,12 @@ export default function Landing(){
 									</div>
 								</div>
 							</div>
-							<div className ="carousel-item bg_cover active carousel-item-left" style = {{backgroundImage: `linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,.5)), url(/images/pp4.jpg)`}}>
+							<div className ="carousel-item bg_cover" style = {{backgroundImage: `linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,.5)), url(/images/un-flags.jpg)`}}>
 								<div className ="carousel-caption po-rel top-0 left-0 h-100 w-100 flex-v a-i-c j-c-c">
 									<div className ="container">
-										<div className ="row justify-content-center-">
-											<div className ="theme-thick-border-left col-lg-9 col-sm-12 text-left">
-												<h1 className = 'bold text-white resize-font'>Passion for Peace Initiative</h1>
-												<h5 className = 'text-white resize-font'>The One Voice Project for Nigeria</h5>
-												<a href = 'https://ppi-africa.com' style = {{border: '2px solid #ec3237'}} className = 'px-5 py-3 text-white bg-orange btn d-inline-block mt-5 text-uppercase fo-s-18'>see more</a>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div className ="carousel-item bg_cover carousel-item-next carousel-item-left" style = {{backgroundImage: `linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,.5)), url(/images/un-flags.jpg)`}}>
-								<div className ="carousel-caption po-rel top-0 left-0 h-100 w-100 flex-v a-i-c j-c-c">
-									<div className ="container">
-										<div className ="row justify-content-center-">
-											<div className ="theme-thick-border-left col-lg-9 col-sm-12 text-left">
-												<h1 className = 'bold text-white resize-font'>PPI Grant 2022</h1>
+										<div className ="row justify-content-center px-4">
+											<div className ="theme-thick-border-left py-4 col-lg-9 col-sm-12 text-left">
+												<h4 className = 'bold text-white resize-font'>PPI Grant 2022</h4>
 												<h5 className = 'text-white resize-font'>PPI is a proud supporter of the UN SDG</h5>
 												<a href = 'https://sdgs.un.org/goals' style = {{border: '2px solid #ec3237'}} className = 'px-5 py-3 text-white bg-orange btn d-inline-block mt-5 text-uppercase fo-s-18'>see sdg goals</a>
 											</div>
@@ -71,14 +71,6 @@ export default function Landing(){
 				</div>
 			</div>
 			<style>{`
-				.resize-font{
-					font-size: 3rem;
-				}
-				@media (min-width: 760px){
-					.resize-font{
-						font-size: 4.5vw;
-					}
-				}
 				.theme-thick-border-left{
 					border-left: 5px solid #ec3237;
 				}
